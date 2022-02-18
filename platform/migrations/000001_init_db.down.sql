c8d5ae57b4b88491b3d25128424ecc74f7553579
Gbolu Adeyemi <gboluwagaadeyemi@gmail.com>
2022-02-18 09:33:14 +0100

    Feature: Prep server

---
+++ platform/migrations/000001_init_db.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS attendees;
+DROP TABLE IF EXISTS talks;
+DROP TABLE IF EXISTS conferences;
+DROP TABLE IF EXISTS edits;
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
+
+DROP TYPE IF EXISTS attendee_type;
+
+DROP TYPE IF EXISTS edit_type;
